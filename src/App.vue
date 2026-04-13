32de4130115e012c36ed532efd21d0f6b469fb7e
LLZZHH <3177358178@qq.com>
2026-04-14 00:52:35 +0800

    搭框架1

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <script setup>
-    import UserInfo from './components/UserInfo.vue';
-    import PhotoPage from './view/PhotoPage.vue';
-    import AlbumPage from './view/AlbumPage.vue';
-    import RecyclePage from './view/RecyclePage.vue';
+    // import UserInfo from './components/UserInfo.vue';
+    // import PhotoPage from './view/PhotoPage.vue';
+    // import AlbumPage from './view/AlbumPage.vue';
+    // import RecyclePage from './view/RecyclePage.vue';
+
+    import { ref } from 'vue'
+    const tabPosition = ref('left')
 </script>
 
 <template>
-    
+  <div class="common-layout">
+    <el-container>
+        <el-header height="5vh" >Header</el-header>
+
+        <el-tabs :tab-position="tabPosition" style="height: 95vh" class="main-tabs">
+            <el-tab-pane label="User">User</el-tab-pane>
+            <el-tab-pane label="Config">Config</el-tab-pane>
+            <el-tab-pane label="Role">Role</el-tab-pane>
+        </el-tabs>
+
+    </el-container>
+  </div>
 </template>
 
-<style scoped>
+<style>
+    .main-tabs > .el-tabs__content {
+        padding: 32px;
+        color: #6b778c;
+        font-size: 32px;
+        font-weight: 600;
+    }
 
+    .el-tabs--right .el-tabs__content,
+    .el-tabs--left .el-tabs__content {
+        height: 100%;
+    }
 </style>
-
--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 
 const app = createApp(App)
 
+for (const[name, comp] of Object.entries(ElementPlusIconsVue)) {
+  app.component(name, comp);
+}
 app.use(ElementPlus)
 app.mount('#app')
